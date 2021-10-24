201636968070cef381bf059e0a876a7fb1ce276e
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2021-10-24 22:34:25 +0500

    add search code

--- js/index.js
+++ js/index.js
@@ -1,30 +1,95 @@
+
+
+ // console.log(fetch("https://www.omdbapi.com/?s=marvel&apikey=9fcd4d84"));
+
+
+// function getData(page, value) {
+//   fetch(`https://www.omdbapi.com/?apikey=9fcd4d84&s=${inputValue}&page=${page}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+
+//       elLoader.style.display = "none";
+//       working(data.Search);
+//     });
+
+//   function working(array) {
+//     array.forEach((element) => {
+//       renderFilms(element);
+//     });
+//   }
+// }
+
+
+// elNext.addEventListener("click", nextPage);
+// function nextPage() {
+//   elFilmsList.innerHTML = "";
+//   page++;
+//   getData(page, inputValue);
+// }
+
+// elPrev.addEventListener('click', prevPage);
+// function prevPage() {
+//   elFilmsList.innerHTML = "";
+//   if (page>1) {
+//     page--;
+//   }
+//   getData(page, inputValue);
+// }
+
+// getData(page, inputValue);
+
+// =========================================
+
 const elLoader = document.querySelector(".js-loader");
+const elSearchForm = document.querySelector(".js-search-form");
+const elSearchInput = elSearchForm.querySelector(".js-search-input");
 const elFilmsList = document.querySelector(".js-films-list");
 const elPrev = document.querySelector(".js-prev");
 const elNext = document.querySelector(".js-next");
-const elFormSearch = document.querySelector(".form-js");
-const elInputSearch = elFormSearch.querySelector(".js-input-search");
 
 let page = 1;
 
-// console.log(fetch("http://www.omdbapi.com/?s=marvel&apikey=9fcd4d84"));
-// elFormSearch.addEventListener("submit", function() {
-//   const inputValue = elInputSearch.value.trim();
-//   elFilmsList.innerHTML = "";
+elLoader.style.display = "none";
 
-  var film = prompt("Qaysi filmni izladiz", "Home Alone").trim();
+elSearchForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  elFilmsList.innerHTML = "";
 
-function getData(page) {
-  fetch(`https://www.omdbapi.com/?apikey=9fcd4d84&s=${film}&page=${page}`)
+  elLoader.style.display = "block";
+  const inputValue = elSearchInput.value.trim();
+
+  getData(page, inputValue);
+});
+
+function getData(page, value) {
+  fetch(`http://www.omdbapi.com/?apikey=d83224c7&s=${value}&page=${page}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      console.log(data.totalResults);
+      if (page <= 1) {
+        elPrev.disabled = true;
+      }
+      if (page > 1) {
+        elPrev.disabled = false;
+      }
+      if (page == Math.ceil(data.totalResults / 10)) {
+        elNext.disabled = true;
+      }
+      if (page < Math.ceil(data.totalResults / 10)) {
+        elNext.disabled = false;
+      }
+      if (Math.ceil(data.totalResults / 10) < 2) {
+        elPrev.disabled = true;
+        elNext.disabled = true;
+      }
 
       elLoader.style.display = "none";
-      working(data.Search);
+      turnFilms(data.Search);
     });
 
-  function working(array) {
+  function turnFilms(array) {
     array.forEach((element) => {
       renderFilms(element);
     });
@@ -32,7 +97,7 @@ function getData(page) {
 }
 
 function renderFilms(object) {
-  const newLi = document.createElement("li");
+    const newLi = document.createElement("li");
   const newImg = document.createElement("img");
   const newTilte = document.createElement("h4");
   const newYear = document.createElement("p");
@@ -40,7 +105,6 @@ function renderFilms(object) {
   newYear.setAttribute("class", "movies-year");
   newTilte.textContent = object.Title;
   newTilte.setAttribute("class", "movies-title mt-3");
-  // newImg.classList = "new-img-js";
   newImg.setAttribute("class", "new-img-js mt-2 d-inline-block");
   newImg.src = object.Poster;
   newImg.width =280;
@@ -54,22 +118,25 @@ function renderFilms(object) {
   elFilmsList.appendChild(newLi);
 }
 
-elNext.addEventListener("click", nextPage);
 function nextPage() {
-  elFilmsList.innerHTML = "";
-  page++;
-  getData(page);
-}
+  page = page + 1;
 
-elPrev.addEventListener('click', prevPage);
+  elPrev.disabled = false;
+  elFilmsList.innerHTML = "";
+  elLoader.style.display = "block";
+  const inputValue = elSearchInput.value.trim();
+  getData(page, inputValue);
+}
+elNext.addEventListener("click", nextPage);
+
 function prevPage() {
+  page = page - 1;
+
   elFilmsList.innerHTML = "";
-  if (page>1) {
-    page--;
-  }
-  getData(page);
+  elLoader.style.display = "block";
+  const inputValue = elSearchInput.value.trim();
+  getData(page, inputValue);
 }
+elPrev.addEventListener("click", prevPage);
 
-getData(page);
 
-// });
